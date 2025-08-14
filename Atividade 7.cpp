ef79082d0078d01f94a66286541ca8fc0b5a22b4
LUCAS FERNANDO MARINS PEIXOTO <lucas.peixoto@catolicasc.edu.br>
2025-08-13 22:11:33 -0300

    Atividade 7 (preciso terminar)

---
+++ Atividade 7.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+	
+	float conversor (float metros){
+		
+		float centimetros = metros * 100;
+		return centimetros;
+}
+	
+	int main (void){
+	
+		float metros;
+	
+		printf ("Digite seu valor em metros: \n");
+		scanf("%.2f", &metros);
+		
+		float centimetros = conversor(metros);
+		
+		return printf ("O valor de %.2f metros em centimetros e %.2f centimetros", metros, centimetros);
+}
